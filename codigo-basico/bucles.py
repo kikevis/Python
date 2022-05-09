6976a5c010b95e4e9cb347e22c9e80a0cc9ecb44
kikevis <villasanchezg22@gmail.com>
2022-05-09 17:58:05 -0500

    bucles

--- codigo-basico/bucles.py
+++ codigo-basico/bucles.py
@@ -1,6 +1,3 @@
-###########################################################
-
-
 def run(num, rept):
     if num <= rept:
         cont = num
@@ -15,44 +12,36 @@ if __name__ == "__main__":
     run(0, repeticiones)
 
 
-###########################################################
+#def potencia(numero):
+#    potencia = 1
+#    while (potencia <= 10):
+#        result = numero ** potencia
+#        print('Potencia de {} elevado a la {} es {}'.format(numero, potencia, result))
+#        potencia += 1
+#
+#
+#def run():
+#    numero = int(input('Escribe el numero al cual quieres averiguarle la potencia: '))
+#    potencia(numero)
+#
+#
+#if __name__ == "__main__":
+#    run()
 
 
-def potencia(numero):
-    potencia = 1
-    while (potencia <= 10):
-        result = numero ** potencia
-        print('Potencia de {} elevado a la {} es {}'.format(numero, potencia, result))
-        potencia += 1
-
-
-def run():
-    numero = int(input('Escribe el numero al cual quieres averiguarle la potencia: '))
-    potencia(numero)
-
-
-if __name__ == "__main__":
-    run()
-
-
-###########################################################
-
-
-def run():
-    print("""
-    ELEVAR NUMERO BASE A N POTENCIAS LIMITE DE RESULTADO 1000
-    """)
-    LIMITE = 1000
-    base = int(input('Ingrese numero base: '))
-
-    contador = 0
-    resultado = base**contador
-
-    while resultado < LIMITE:
-        print('{} elevado a la {} es: {}'.format(base,contador,resultado))
-        contador+=1
-        resultado = base**contador
-
-
-if __name__ == '__main__':
-    run()
+#def run():
+#    print(" ELEVAR NUMERO BASE A N POTENCIAS LIMITE DE RESULTADO 1000")
+#    LIMITE = 1000
+#    base = int(input('Ingrese numero base: '))
+#
+#    contador = 0
+#    resultado = base**contador
+#
+#    while resultado < LIMITE:
+#        print('{} elevado a la {} es: {}'.format(base,contador,resultado))
+#        contador+=1
+#        resultado = base**contador
+#
+#
+#if __name__ == '__main__':
+#    run()
