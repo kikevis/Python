4eb988804396d3e9ec009d93c7912d52d542fefd
kikevis <villasanchezg22@gmail.com>
2022-05-10 10:21:26 -0500

    bucles

--- codigo-basico/bucles.py
+++ codigo-basico/bucles.py
@@ -1,15 +1,14 @@
-def run(num, rept):
-    if num <= rept:
-        cont = num
-        print(str(2 ** cont))
-        run(num+1, rept)
-    else:
-        print("Fin!")
-
-
-if __name__ == "__main__":
-    repeticiones = int(input("Cuantas potencias: "))
-    run(0, repeticiones)
+#def run(num, rept):
+#    if num <= rept:
+#        cont = num
+#        print(str(2 ** cont))
+#        run(num+1, rept)
+#    else:
+#        print("Fin!")
+#
+#if __name__ == "__main__":
+#    repeticiones = int(input("Cuantas potencias: "))
+#    run(0, repeticiones)
 
 
 #def potencia(numero):
@@ -24,24 +23,22 @@ if __name__ == "__main__":
 #    numero = int(input('Escribe el numero al cual quieres averiguarle la potencia: '))
 #    potencia(numero)
 #
-#
 #if __name__ == "__main__":
 #    run()
 
 
-#def run():
-#    print(" ELEVAR NUMERO BASE A N POTENCIAS LIMITE DE RESULTADO 1000")
-#    LIMITE = 1000
-#    base = int(input('Ingrese numero base: '))
-#
-#    contador = 0
-#    resultado = base**contador
-#
-#    while resultado < LIMITE:
-#        print('{} elevado a la {} es: {}'.format(base,contador,resultado))
-#        contador+=1
-#        resultado = base**contador
-#
-#
-#if __name__ == '__main__':
-#    run()
+def run():
+    print("ELEVAR NUMERO BASE A N POTENCIAS LIMITE DE RESULTADO 1000")
+    LIMITE = 1000
+    base = int(input('Ingrese numero base: '))
+
+    contador = 0
+    resultado = base**contador
+
+    while resultado < LIMITE:
+        print('{} elevado a la {} es: {}'.format(base,contador,resultado))
+        contador+=1
+        resultado = base**contador
+
+if __name__ == '__main__':
+    run()
